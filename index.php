cd12729493cad606da804bc64dcacaffbf3941c9
ahmadmaulana10 <ahmadmaulana.am96@gmail.com>
2019-06-29 22:42:39 +0700

    menambahkan gambar, menghapus file yang tdk diperlukan

--- index.php
+++ index.php
@@ -6,12 +6,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="asset/css/bootstrap.min.css">
+<link rel="stylesheet" href="asset/css/bootstrap.css">
 <link rel="stylesheet" href="asset/css/style.css">
 
-<title>Apotek</title>
-</head>
-<body>
 <?php
 if(isset($_GET["p"])){
     $page = $_GET["p"];
@@ -19,8 +16,8 @@ if(isset($_GET["p"])){
         case "beranda":
             $page = "beranda";
             break;
-        case "ga":
-            $page = "ga";
+        case "galeri":
+            $page = "galeri";
             break;
         case "testimoni":
             $page = "testimoni";
@@ -29,21 +26,25 @@ if(isset($_GET["p"])){
             $page = "kontak_kami";
             break;
     }
-    
 }
 ?>
+<title>Apotek</title>
+</head>
+<body>
+
 <!-- Navbar -->
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="index.php?p=beranda">
             <img src="asset/img/logo_apotek.png" width="75" height="75" alt="Logo Apotek">
-            APOTEK SYUKUR</a>
+            <span style="font-weight:bold">APOTEK SYUKUR</span>
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item  <?php if($page==="beranda" or $page == "") echo "active";?>">
+                <li class="nav-item <?php if($page==="beranda" or $page == "") echo "active";?>">
                     <a class="nav-link" href="index.php?p=beranda">BERANDA <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item <?php if($page === "galeri") echo "active";?>">
@@ -91,8 +92,8 @@ if(isset($_GET["p"])){
                     case 'kontak_kami':
                         include "kontak_kami.php";
                         break;
-                    default :
-                        echo '<h1 class="btn-danger">Halaman yang anda cari tidak ada !</h1>';
+                    default:
+                        echo '<h1 class="btn-danger m-5">Halaman yang anda cari tidak ada !</h1>';
                         break;
                 };
             }else{
--- testimoni.php
+++ testimoni.php
@@ -1,73 +1,65 @@
-<div class="row">
-    <div class="col-md">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<div class="row mt-5">
+    <div class="col-lg-12 col-md-offset-6">
+        <div class="card-deck mt-5">
+            <div class="card">
+                <img src="asset/img/testi_1.jpg" height="150" width="100" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aldi James</h5>
+                    <p class="card-text">Senang belanja obat disini, kasirnya cantik, boleh minta nomornya ga?</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Mahasiswa</small>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card">
+                <img src="asset/img/testi_2.jpg" height="150" width="100" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Niko Cage</h5>
+                    <p class="card-text">Gak nyesel dah beli disini !</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Aktor</small>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="col-md">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="card">
+                <img src="asset/img/testi_3.jpg" height="150" width="100" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mang Gaben</h5>
+                    <p class="card-text">Kolesterolku turun setelah konsultasi dengan dokter disini !</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Miliarder</small>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card">
+                <img src="asset/img/testi_4.jpg" height="150" width="100" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Kang Kojima</h5>
+                    <p class="card-text">100% ampuh obatnya !</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Orang Jepang</small>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="col-md">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="card">
+                <img src="asset/img/testi_5.jpg" height="150" width="100" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mas Toni</h5>
+                    <p class="card-text">Terimakasih, Avenger sangat terbantu, encok steve roger bisa sembuh.</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">CEO Stark Industries</small>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-    </div>
-    <div class="col-md">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-    </div>
-    <div class="col-md">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-    </div>
-    <div class="col-md">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
+            <div class="card">
+                <img src="asset/img/testi_6.jpg" height="150" width="100" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Neng Dea</h5>
+                    <p class="card-text">Bisul aku sembuh, beli obat disini, terimakasih tong-fang !</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Mahasiswi</small>
+                </div>
             </div>
         </div>
     </div>
