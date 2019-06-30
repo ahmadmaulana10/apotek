d88e662a3903779f36d8f3bb799740627e265776
ahmadmaulana10 <ahmadmaulana.am96@gmail.com>
2019-06-30 16:17:47 +0700

    fixed testimoni

--- galeri.php
+++ galeri.php
@@ -1,32 +1,32 @@
 <div class="bd-example">
-  <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-ride="carousel">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="asset/img/boy.png" class="d-block w-20" alt="...">
-        <div class="carousel-caption d-lg-block">
-          <h5>First slide label</h5>
+        <img src="asset/img/slide_1.jpg" height="400" class="d-block w-50 m-auto" alt="...">
+        <div style="margin-top:150px;" class="carousel-caption d-none d-md-block">
+          <h5>Paramex Flu & Batuk</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="asset/img/child_girl.png" class="d-block w-20" alt="...">
-        <div class="carousel-caption d-lg-block">
-          <h5>First slide label</h5>
+        <img src="asset/img/slide_2.jpg" height="400" class="d-block w-50 m-auto" alt="...">
+        <div style="margin-top:150px;" class="carousel-caption d-none d-md-block">
+          <h5>Paramex</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="asset/img/slide_3.png" height="400" class="d-block w-50 m-auto" alt="...">
+        <div style="margin-top:150px;" class="carousel-caption d-none d-md-block">
+          <h5>Paramex</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </div>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
   </div>
 </div>
